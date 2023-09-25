f26d0240c62901b8f9cf7cf5fcc7a04a5ae2e603
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2023-09-24 21:13:05 -0300

    EnPassant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,16 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
+	private ChessMatch chessMatch;
 
-	public Pawn(Board board, Color color) {
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -34,8 +37,23 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				matrix[p.getRow()][p.getColumn()] = true;
 			}
-		}
-		else {
+
+			// #Specialmove enPassant white
+			if (position.getRow() == 3) {
+				Position left_position = new Position(position.getRow(), position.getColumn() - 1);
+				if (getBoard().positionExists(left_position) && isThereOpponentPiece(left_position)
+						&& getBoard().piece(left_position) == chessMatch.getEnPassantVulnerable()) {
+					matrix[left_position.getRow() - 1][left_position.getColumn()] = true;
+				}
+			}
+			if (position.getRow() == 3) {
+				Position right_position = new Position(position.getRow(), position.getColumn() + 1);
+				if (getBoard().positionExists(right_position) && isThereOpponentPiece(right_position)
+						&& getBoard().piece(right_position) == chessMatch.getEnPassantVulnerable()) {
+					matrix[right_position.getRow() - 1][right_position.getColumn()] = true;
+				}
+			}
+		} else {
 			p.setRowColumnValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				matrix[p.getRow()][p.getColumn()] = true;
@@ -54,9 +72,26 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				matrix[p.getRow()][p.getColumn()] = true;
 			}
+
+			// #Specialmove enPassant black
+			if (position.getRow() == 4) {
+				Position left_position = new Position(position.getRow(), position.getColumn() - 1);
+				if (getBoard().positionExists(left_position) && isThereOpponentPiece(left_position)
+						&& getBoard().piece(left_position) == chessMatch.getEnPassantVulnerable()) {
+					matrix[left_position.getRow() + 1][left_position.getColumn()] = true;
+				}
+			}
+			if (position.getRow() == 3) {
+				Position right_position = new Position(position.getRow(), position.getColumn() + 1);
+				if (getBoard().positionExists(right_position) && isThereOpponentPiece(right_position)
+						&& getBoard().piece(right_position) == chessMatch.getEnPassantVulnerable()) {
+					matrix[right_position.getRow() + 1][right_position.getColumn()] = true;
+				}
+			}
 		}
 		return matrix;
 	}
+
 	@Override
 	public String toString() {
 		return "P";
